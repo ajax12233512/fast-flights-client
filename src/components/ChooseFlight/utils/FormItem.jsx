36a2148e1890dc15160bfeac6be93fdfeef5393b
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2022-07-10 02:16:28 -0500

    added calendar for input

--- src/components/ChooseFlight/utils/FormItem.jsx
+++ src/components/ChooseFlight/utils/FormItem.jsx
@@ -2,15 +2,27 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { setLeaveDate } from '../../../app/BookingStates/datesStates/leaveDateSlice'
 import { setReturnDate } from '../../../app/BookingStates/datesStates/returnDateSlice'
+import Calendar from 'react-calendar'
 
 function FormItem(props) {
 
+  const [showCalendar, setShowCalendar] = React.useState(false);
   const dispatch = useDispatch()
   const handleType = (e) => {
     const value = e.target.value
     const id = e.target.attributes.id.value
     if(id === 'return-input') dispatch(setReturnDate(value)) 
     else dispatch(setLeaveDate(value))
+  }
+
+  const toggleCalendar = (e) => {
+    e.preventDefault();
+    setShowCalendar(true);
+  }
+
+  const hideCalendar = (e) => {
+    e.preventDefault();
+    setShowCalendar(false);
   }
 
   return (
@@ -21,7 +33,14 @@ function FormItem(props) {
         className='cf-form-input' 
         type='text' 
         placeholder={props.placeholder}
+        onFocus={toggleCalendar}
+        onBlur={hideCalendar}
       />
+      <div style={
+        showCalendar ? {display: 'block'} : {display: 'none'}
+      }>
+        <Calendar />
+      </div>
     </div>
   )
 }

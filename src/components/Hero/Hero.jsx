74c7205db4e63d6d92d6772ffd0f3d890413888f
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2022-07-14 20:23:40 -0500

    starting major home page refactor

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Hero.css'
+function Hero() {
+    return (
+        <div class="booking-cta">
+            <h1>Book your flight today</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate laboriosam numquam at</p>
+        </div>
+    )
+}
+
+export default Hero
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,25 @@ import Header from '../../components/Header/Header';
 import Menu from '../../components/Menu/Menu';
 import ChooseFlight from '../../components/ChooseFlight/ChooseFlight';
 import './Home.css';
-
+import Hero from '../../components/Hero/Hero';
+import BookingForm from '../../components/BookingForm/BookingForm';
 function Home() {
   
   return (
-    <div className='home'>
-        <ChooseFlight />
-    </div>
+    <div id="booking" class="section">
+		<div class="section-center">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<Hero />
+					</div>
+					<div class="col-md-7 col-md-offset-1">
+						<BookingForm />
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
   )
 }
 
